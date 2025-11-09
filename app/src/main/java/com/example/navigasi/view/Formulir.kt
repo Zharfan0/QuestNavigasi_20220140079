920b8ff322f728cd0ca6f627c5cf9f3943616e01
Zharfan <zharfanwidyadana@gmail.com>
2025-11-09 10:10:23 +0700

    membuat variabel pada fungsi FormIsian

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -1,2 +1,10 @@
 package com.example.navigasi.view
 
+import android.R
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+  JenisK:List<String>   = listOf("Laki-laki","Perempuan"),
+  OnSubmitBtnClick : () -> Unit
+){}
