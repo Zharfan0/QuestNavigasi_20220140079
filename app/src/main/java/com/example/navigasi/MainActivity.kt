3d82c77ecc2221d1999ccec39ed0b05305b01a61
Zharfan <zharfanwidyadana@gmail.com>
2025-11-09 10:00:32 +0700

    membuat (MainActivity) override fun dan @SupressLInt

--- app/src/main/java/com/example/navigasi/MainActivity.kt
+++ app/src/main/java/com/example/navigasi/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.navigasi
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
-
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
