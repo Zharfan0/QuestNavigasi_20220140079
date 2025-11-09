751ebb0750eac559d0f2f1c3a14dfc987870ba23
Zharfan <zharfanwidyadana@gmail.com>
2025-11-09 10:03:02 +0700

    MainActivity.kt Clear

--- app/src/main/java/com/example/navigasi/MainActivity.kt
+++ app/src/main/java/com/example/navigasi/MainActivity.kt
@@ -4,10 +4,26 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import com.example.navigasi.ui.theme.NavigasiTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigasiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    //kode program bagian yang diedit
+                    DataApp(modifier = Modifier)
+                }
+            }
+        }
     }
 }
